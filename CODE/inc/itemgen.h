e26276640ced622da55ee815b4d7ed50f0e4d4fc
Shaun Brandt <shaunbr@gmail.com>
2025-01-11 11:10:40 -0800

    Rototill is complete.  Still need to make sure nothing weird is happening

--- CODE/inc/itemgen.h
+++ CODE/inc/itemgen.h
@@ -63,7 +63,7 @@ namespace ItemConsts {
     // 
     // Note: key items will be handled separately.
     const int g_item_class_pool[]= {10, 48, 63, 78, 98, 100};
-    //const int g_item_class_pool[]= {10, 48, 63, 78, 79, 100};
+    // const int g_item_class_pool[]= {10, 48, 63, 78, 79, 100};
     const int g_item_class_pool_count = 6;
     const int g_item_class_pool_entries = 100;
 
